9cadef39a93cf44b6b02cc079654bd080258e94a
allan-gilber <allangilber@yahoo.com.br>
2022-02-19 23:28:57 -0300

    feat: criado menu lateral.

--- hackifood/src/App.tsx
+++ hackifood/src/App.tsx
@@ -1,11 +1,17 @@
 import Menu from './components/Menu';
 import StylesGlobal  from './styles/GlobalStyles.jsx'
+import {Layout, message} from 'antd';
+import { Header } from 'antd/lib/layout/layout';
 
 export function App() {
+  
   return (
-    <div>
+    <div style={{display: 'flex', flexDirection: 'row',}}>
+  
+      <Layout  style={{ height: '100vh', background: '#fff'}} >
       <StylesGlobal />
       <Menu/>
+      </Layout>
     </div>
   ); 
 } 
